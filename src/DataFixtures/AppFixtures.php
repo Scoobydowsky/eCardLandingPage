f8eaf701ac7805ad5a7e3984811c6eef42498eff
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2024-04-18 20:23:15 +0200

    Update AppFixtures.php
    Added Load User to Database

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,37 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher
+    )
+    {
+
+    }
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
+        $plainPassword= 'admin';
 
+        $user = new User();
+        $hashedPassword=  $this->passwordHasher->hashPassword($user, $plainPassword);
+        $user->setUsername('admin')
+            ->setName('Admin')
+            ->setSurname('Adminowsky')
+            ->setNick('Admino')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setDescription('Hello Word')
+            ->setPassword($hashedPassword);
+        $manager->persist($user);
         $manager->flush();
     }
 }
