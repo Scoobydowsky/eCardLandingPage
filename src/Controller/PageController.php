03b205201992ae0658f32e2922769560d8dbd007
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2024-04-18 20:55:20 +0200

    Update PageController.php
    Added vars where get data from DB

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,17 +3,30 @@
 namespace App\Controller;
 
 use App\Controller\PageInterface;
+use App\Service\DataGetterService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PageController extends AbstractController implements PageInterface
 {
+    public function __construct(
+        Private DataGetterService $getterService,
+    )
+    {
+
+    }
 
     #[Route('/',name:'app_homepage')]
     public function renderHomepage()
     {
+        $userData = $this->getterService->getUserData();
+        $profiles = $this->getterService->getSocials();
 
-
-        return $this->render('homepage.html.twig');
+        dump($profiles);
+        return $this->render('homepage.html.twig',
+        [
+            'user'=>$userData,
+            'profiles'=>$profiles
+        ]);
     }
 }
