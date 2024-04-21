d7315e4dc4321d8f2ff6fd521f0d053b3ec38875
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2024-04-21 22:05:20 +0200

    Update SocialService.php
    Added service to delete social profile links

--- src/Service/SocialService.php
+++ src/Service/SocialService.php
@@ -11,4 +11,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SocialService extends AbstractController
 {
+    public function __construct(
+        private LinksRepository $repository,
+        private EntityManagerInterface $entityManager
+    )
+    {
+
+    }
+    public function deleteSocial(int $id):bool
+    {
+        $socialProfile =$this->repository->findOneBy(['id'=>$id]);
+        if ($socialProfile === null) {
+            $exception = new EntityNotFoundException();
+            $this->addFlash('error','Nie znaleziono linku o podanym ID' . $exception->getMessage());
+            return false;
+        }
+        try {
+            $this->entityManager->remove($socialProfile);
+            $this->entityManager->flush();
+        }catch (ORMException $exception){
+            echo $exception->getMessage();
+            return false;
+        }
+        catch (\Exception $exception){
+            echo $exception->getMessage();
+        }
+        $this->addFlash('success','Pomyślnie usunięto link ');
+        return true;
+    }
 }
