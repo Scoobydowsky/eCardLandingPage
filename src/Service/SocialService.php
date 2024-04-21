82554a51ffab14482f9211f3ea74837d9eefee41
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2024-04-21 22:00:45 +0200

    Create SocialService.php

---
+++ src/Service/SocialService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\LinksRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityNotFoundException;
+use Doctrine\ORM\Exception\ORMException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class SocialService extends AbstractController
+{
+}
